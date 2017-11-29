c5c08b4f35ae928e9390c8595d608c7b5af0e6e4
Eneias Cordeiro da Silva <eneiascs@gmail.com>
2017-11-29 16:58:28 +0100

    Renamed files

--- haskell/Dohko.hs
+++
@@ -1,9 +0,0 @@
-module Dohko where
-import System.Random
-data ApplicationDescriptor = ApplicationDescriptor {applications :: [Application]} deriving (Show, Eq, Ord) 
-data Application = Application {appInstrument :: String, appCommand :: String, appArgument:: String} deriving (Show, Eq, Ord) 
-
-dohko :: ApplicationDescriptor -> [IO Float]
-dohko applicationDescriptor =map execute (applications applicationDescriptor) 
-execute :: Application -> IO Float
-execute application = getStdRandom (randomR (1,1000)) 
